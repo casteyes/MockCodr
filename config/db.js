72fcdf8f4e7023f551b1eb2dff55b73728f42755
Casteyes <casteyestothesun@gmail.com>
2021-07-05 22:33:01 -0400

    Added user model

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const config = require('config');
+const gravatar = require('gravatar')
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try{
     await mongoose.connect(db,{
-        useNewUrlParser:true
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useUnifiedTopology: true
+
     });
     console.log('Mongo DB Connected...')
     }catch (err){
