969c17d5d8322b483b371e768e19ceff06a8aef5
Casteyes <casteyestothesun@gmail.com>
2021-07-15 23:06:06 -0400

    Finished profile routes and complete Post.js model

--- Routes/API/posts.js
+++ Routes/API/posts.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route GET api/posts
-// @ desc Test Route
-// @access Public
-router.get ('/', (req,res) => res.send('Posts Route'));
+
+
+// @route POST api/posts
+// @ desc Create a post
+// @access Private
+router.post ('/',[auth, [check('text', 'Text is required').not().isEmpty() ]], (req,res) => {
+    
+});
 
 module.exports = router;
