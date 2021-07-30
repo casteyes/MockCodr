de04d138c6a30b16765565ee081fa4776067250e
Casteyes <casteyestothesun@gmail.com>
2021-07-29 20:22:40 -0400

    Implemented bug fixes.

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -2,18 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 
-const ProflieEducation = ({education:{company, title, location, current, to, from, description}}) => 
+const ProflieEducation = ({education:{school, degree, fieldofstudy, current, to, from, description}}) => 
     
         <div>
-            <h3 className="text-dark">{company}</h3>
+            <h3 className="text-dark">{school}</h3>
             <p>
                 <Moment format='YYYY/MM/DD'>{from}</Moment> - {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
             </p>
             <p>
-                <strong>Position:</strong> {title}
+                <strong>Degree:</strong> {degree}
             </p>
             <p>
                 <strong>Description:</strong> {description}
+            </p>
+            <p>
+                <strong>fieldofstudy:</strong> {fieldofstudy}
             </p>
         </div>
 
@@ -24,3 +27,4 @@ education: PropTypes.array.isRequired,
 }
 
 export default ProflieEducation
+
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,7 +10,8 @@ import EditProfile from '../profile-forms/EditProfile';
 import AddExperience from '../profile-forms/AddExperience';
 import AddEducation from '../profile-forms/AddEducation';
 import Profiles from '../profiles/Profiles';
-import Profile from '../profile/Profile'
+import Profile from '../profile/Profile';
+import Posts from '../posts/Posts'
 
 
 
@@ -29,6 +30,7 @@ const Routes = props => {
             <PrivateRoute exact path="/Edit-Profile" component={EditProfile} />
             <PrivateRoute exact path="/Add-Experience" component={AddExperience} />
             <PrivateRoute exact path="/Add-Education" component={AddEducation} />
+            <PrivateRoute exact path="/Posts" component={Posts} />
         </Switch>
       </section>
     );
